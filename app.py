9da9dc9219a54f8afad8407bd23a6107b5ada12e
Sebastien VEY <sebvey@gmail.com>
2022-02-25 14:15:32 +0100

    Prediction is now done only when Predict button is clicked

--- app.py
+++ app.py
@@ -15,46 +15,48 @@ def main():
 
     # Station Selectbox (station_id selection)
     with col1 :
+
         station_id = st.selectbox('Station to Predict :',
-                              stations.index,
-                              format_func=lambda i: stations.loc[i, 'label'])
+                            stations.index,
+                            format_func=lambda i: stations.loc[i, 'label'])
 
-    label = stations.loc[station_id, 'label'].capitalize()
-    river = stations.loc[station_id, 'river_label']
-    alt = stations.loc[station_id, 'alt']
-    lat = round(float(stations.loc[station_id, 'lat']),3)
-    lon = round(float(stations.loc[station_id, 'lon']), 3)
-    coord = f"({lat},{lon})"
+        label = stations.loc[station_id, 'label'].capitalize()
+        river = stations.loc[station_id, 'river_label']
+        alt = stations.loc[station_id, 'alt']
+        lat = round(float(stations.loc[station_id, 'lat']),3)
+        lon = round(float(stations.loc[station_id, 'lon']), 3)
+        coord = f"({lat},{lon})"
 
-
-    with col1 :
         desc = f"""🌊 River : {river} | ⛰ Altitude : {alt}m |  Coordinates : {coord}"""
         st.write(desc)
+        predict_asked = st.button('Predict')
+
 
     with col2 :
         st.pydeck_chart(maps.get_deck(stations, station_id))
 
     weather,prediction = util.get_predictions_dfs(station_id)
 
-    with col1 :
-        pred_title = f"<h3 style='text-align: center;'>Water Quality Prediction at {label}</h3>"
-        st.markdown(pred_title, unsafe_allow_html=True)
-        st.pyplot(plots.prediction_figure(prediction))
+    if predict_asked :
+        with col1 :
+            pred_title = f"<h3 style='text-align: center;'>Water Quality Prediction at {label}</h3>"
+            st.markdown(pred_title, unsafe_allow_html=True)
+            st.pyplot(plots.prediction_figure(prediction))
 
-    st.markdown("""---""")
+        st.markdown("""---""")
 
-    l,center,r = st.columns([1,3,1])
+        l,center,r = st.columns([1,3,1])
 
-    with center :
-        w_title = "<h2 style='text-align: center;'>"\
-            "Weather Data Used by the Model</h2>"
-        st.markdown(w_title, unsafe_allow_html=True)
-        st.pyplot(plots.weather_figure(weather))
+        with center :
+            w_title = "<h2 style='text-align: center;'>"\
+                "Weather Data Used by the Model</h2>"
+            st.markdown(w_title, unsafe_allow_html=True)
+            st.pyplot(plots.weather_figure(weather))
 
-    with l :
-        st.write(" ")
-    with r :
-        st.write(" ")
+        with l :
+            st.write(" ")
+        with r :
+            st.write(" ")
 
 
 ## MAIN CODE -------------------------------------------------------------------
